90f9b771bebecdeafd206a6d975c15d4c05314ed
GRISHENKO <original.grishenko@gmail.com>
2020-12-05 16:26:21 +0100

    Add barcode scan features

--- src/screens/ScanScreen.tsx
+++ src/screens/ScanScreen.tsx
@@ -1,16 +1,24 @@
-import React from 'react';
-import { SafeAreaView, Text, View } from 'react-native';
+import React, { useRef } from 'react';
+import { SafeAreaView } from 'react-native';
+import { RNCamera } from 'react-native-camera';
 
 const ScanScreen: React.FunctionComponent = () => {
+	const camera = useRef(null);
+
 	return (
 		<SafeAreaView
 			style={{
 				flex: 1,
 			}}
 		>
-			<View>
-				<Text>Scan</Text>
-			</View>
+			<RNCamera
+				ref={camera}
+				style={{ height: '100%', width: '100%' }}
+				captureAudio={false}
+				onBarCodeRead={({ data }) => {
+					console.log(data);
+				}}
+			/>
 		</SafeAreaView>
 	);
 };
